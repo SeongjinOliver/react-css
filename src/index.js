bac7e68a7ad5b4f2e1f234ff2afeb562d8e3245e
SeongjinOliver <yoosngj@gmail.com>
2023-01-29 18:38:39 +0900

    Study css from React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
+import StyledComponent from "./components/StyledComponent";
+import TailwindComponent from "./components/TailwindComponent";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <>
         <Button1/>
         <Button2/>
-    </React.StrictMode>
+        <StyledComponent/>
+        <TailwindComponent/>
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
